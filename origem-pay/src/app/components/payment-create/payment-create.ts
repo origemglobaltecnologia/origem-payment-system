4a6bd1f30eb2a75fab6fd1a283bc9f830a2d622d
Origem <origemglobaltecnologia@gmail.com>
2026-03-17 00:18:21 -0300

    feat: integração front e back finalizada e testada

--- origem-pay/src/app/components/payment-create/payment-create.ts
+++ origem-pay/src/app/components/payment-create/payment-create.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { PaymentService } from '../../services/payment.service';
+import { Payment } from '../../models/payment.model';
 
 @Component({
   selector: 'app-payment-create',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './payment-create.html',
   styleUrl: './payment-create.css'
 })
-export class PaymentCreate {}
+export class PaymentCreate {
+  private service = inject(PaymentService);
+  private router = inject(Router);
+
+  // Objeto que será vinculado ao formulário
+  novoPagamento: Payment = {
+    descricao: '',
+    valor: 0,
+    metodo: 'PIX'
+  };
+
+  onSubmit() {
+    this.service.salvar(this.novoPagamento).subscribe({
+      next: () => {
+        alert('Pagamento criado com sucesso!');
+        this.router.navigate(['/payments']); // Volta para a lista
+      },
+      error: (err) => console.error('Erro ao salvar:', err)
+    });
+  }
+}
